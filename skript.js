1add1a50ac934433679d0f189683ac1fd558b6a2
mabay919 <54597981+mabay919@users.noreply.github.com>
2021-03-22 18:45:42 +0200

    fix: body backgroundColor changing

--- skript.js
+++ skript.js
@@ -1,4 +1,23 @@
-var map;
+let checkBox1 = document.getElementById('slide-1'),
+checkBox2 = document.getElementById('slide-2'),
+checkBox3 = document.getElementById('slide-3'),
+body = document.querySelector('body'),
+checkbox1Label = document.querySelector(".label_slide_1"),
+checkbox2Label = document.querySelector(".label_slide_2"),
+checkbox3Label = document.querySelector(".label_slide_3");
+
+checkbox1Label.addEventListener('click', () => {
+  body.style.backgroundColor = '#849d8f'
+});
+checkbox2Label.addEventListener('click', () => {
+  body.style.backgroundColor = '#8996a6'
+});
+checkbox3Label.addEventListener('click', () => {
+  body.style.backgroundColor = '#9d8b84'
+});
+
+
+let map;
 function initMap() {
   // В переменной map создаем объект карты GoogleMaps и вешаем эту переменную на <div id="map"></div>
   map = new google.maps.Map(document.getElementById('map'), {
